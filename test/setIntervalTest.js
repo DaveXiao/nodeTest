f1ebe7e64c544cf3df4aeb1a25136bc0532defca
xiaoxiong <1870598302@qq.com>
2018-08-23 23:27:34 +0800

    clearInterval函数运用

--- test/setIntervalTest.js
+++ test/setIntervalTest.js
@@ -2,4 +2,7 @@ var testFunction = function (msg) {
     console.log(msg)
 }
 //setInterval函数每隔多少秒回调一次函数，第一个参数是回调函数，第二个参数是时间，第三个是回调函数传入的参数
-setInterval(testFunction,1000,"testFunction function executed.")
+var timer = setInterval(testFunction,1000,"testFunction function executed.")
+
+//使用clearInterval函数取消对testFunction回调函数的调用
+clearInterval(timer)
